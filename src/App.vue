8bf6e84ebfafb718d9df3c5a03d9bccf597ab09e
PyWendi <rakotondranaivogilbert21@gmail.com>
2024-08-29 13:07:50 +0200

    initial commit on project repository

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <!-- <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink> -->
+    <RouterView />
+  </div>
+  
+</template>
+
+<style>
+
+</style>
---
+++ src/pages/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+	<div>
+		<p class="text-3xl text-cyan-300">hello world</p>
+	</div>
+</template>
